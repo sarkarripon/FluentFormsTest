6bae8b8430123fad05d83ac2e020b38d5d8f5b9c
Sarkar Ripon <sarkarripon@student.sust.edu>
2023-03-15 16:46:18 +0600

    test helper configuration

--- tests/Acceptance/WpLoginCest.php
+++ tests/Acceptance/WpLoginCest.php
@@ -4,62 +4,33 @@ namespace Tests\Acceptance;
 
 use Tests\Support\AcceptanceTester;
 use Codeception\Util\Locator;
-
 class WpLoginCest
 {
     public function _before(AcceptanceTester $I)
     {
-        $I->wantTo('Login to Wordpress');
-        $I->amOnPage('/wp-login.php');
-        $I->fillField('Username','admin');
-        $I->fillField('Password','admin');
-        $I->click('Log In');
-        $I->see('Dashboard');
+        $I->wpLogin();
     }
 
-    public function Install_fluentForm_plugin(AcceptanceTester $I)
+    public function Install_plugins(AcceptanceTester $I)
     {
-        $I->wantTo('Install FluentForm plugin');
-        $I->amOnPage('wp-admin/plugin-install.php?s=Fluent+Forms+contact+form&tab=search&type=term');
-        $I->click('Install Now', '.plugin-card.plugin-card-fluentform');
-        $I->waitForText('Activate',30,'.plugin-card.plugin-card-fluentform');
-        $I->click('Activate', '.plugin-card.plugin-card-fluentform');
+        $I->installFluentForm();
+        $I->installFluentFormPdfGenerator();
+        $I->installFluentFormPro();
+    }
+
+
+//Main test function goes here
+
+
+
+
+    public function UninstallPlugins(AcceptanceTester $I)
+    {
+        $I->wantTo('Clean up plugin');
         $I->amOnPage('/wp-admin/plugins.php');
-        $I->see('Fluent Forms');
-    }
 
-    public function Install_fluentForm_pdf_generator_plugin(AcceptanceTester $I)
-    {
-        $I->wantTo('Install FluentForm PDF Generator plugin');
-        $I->amOnPage('wp-admin/plugin-install.php?s=Fluent+Forms+PDF+Generator&tab=search&type=term');
-        $I->click('Install Now', '.plugin-card.plugin-card-fluentforms-pdf');
-        $I->waitForText('Activate',30,'.plugin-card.plugin-card-fluentforms-pdf');
-        $I->click('Activate', '.plugin-card.plugin-card-fluentforms-pdf');
-        $I->amOnPage('/wp-admin/plugins.php');
-        $I->see('Fluent Forms PDF Generator');
-    }
-
-    public function Install_fluentFormPro_plugin( AcceptanceTester $I)
-    {
-        $I->wantTo('Install FluentForm Pro plugin');
-        $I->amOnPage('wp-admin/plugin-install.php');
-        $I->seeElement('.upload');
-        $I->click('.upload');
-        $I->attachFile('input[type="file"]','fluentformpro.zip');
-        $I->seeElement('#install-plugin-submit');
-        $I->click('#install-plugin-submit');
-        $I->waitForText('Activate Plugin',30,'.button.button-primary');
-        $I->click('.button.button-primary');
-        $I->waitForText('The Fluent Forms Pro Add On license needs to be activated. Activate Now',30, "div[class='error error_notice_ff_fluentform_pro_license'] p");
-        $I->click('Activate Now', "div[class='error error_notice_ff_fluentform_pro_license'] p");
-        $I->waitForElement("input[name='_ff_fluentform_pro_license_key']",30);
-        $I->fillField("input[name='_ff_fluentform_pro_license_key']",'97019eb28ede80342d3a17da2da5f41d');
-        $I->click("input[value='Activate License']");
-        $I->waitForText('Your license is active! Enjoy Fluent Forms Pro Add On');
-        $I->see('Your license is active! Enjoy Fluent Forms Pro Add On');
-
-//        $I->amOnPage('/wp-admin/plugins.php');
-//        $I->see('Fluent Forms Pro Add On Pack');
-
+        $I->uninstallFluentFormPro();
+        $I->uninstallFluentFormPdfGenerator();
+        $I->uninstallFluentForm();
     }
 }
--- tests/Support/AcceptanceTester.php
+++ tests/Support/AcceptanceTester.php
@@ -24,6 +24,106 @@ class AcceptanceTester extends \Codeception\Actor
     use _generated\AcceptanceTesterActions;
 
     /**
-     * Define custom actions here
+     * Login to WordPress
+     * @return void
      */
+    public function wpLogin()
+    {
+        $this->wantTo('Login to Wordpress');
+        $this->amOnPage('/wp-login.php');
+        $this->fillField('Username','admin');
+        $this->fillField('Password','admin');
+        $this->click('Log In');
+        $this->see('Dashboard');
+    }
+
+    public function installFluentForm()
+    {
+        $this->wantTo('Install FluentForm plugin');
+        $this->amOnPage('wp-admin/plugin-install.php?s=Fluent+Forms+contact+form&tab=search&type=term');
+        if ($this->tryToSee('Install Now', '.plugin-card.plugin-card-fluentform'))
+        {
+            $this->click('Install Now', '.plugin-card.plugin-card-fluentform');
+            $this->waitForText('Activate',30,'.plugin-card.plugin-card-fluentform');
+            $this->click('Activate', '.plugin-card.plugin-card-fluentform');
+        }
+        $this->amOnPage('/wp-admin/plugins.php');
+        $this->see('Fluent Forms');
+    }
+
+    public function installFluentFormPro()
+    {
+        $this->wantTo('Install FluentForm Pro plugin');
+        $this->amOnPage('wp-admin/plugin-install.php');
+        $this->seeElement('.upload');
+        $this->click('.upload');
+        $this->attachFile('input[type="file"]','fluentformpro.zip');
+        $this->seeElement('#install-plugin-submit');
+        $this->click('#install-plugin-submit');
+        $this->waitForText('Activate Plugin',30,'.button.button-primary');
+        $this->click('.button.button-primary');
+        if($this->tryToSee('The Fluent Forms Pro Add On license needs to be activated. Activate Now', "div[class='error error_notice_ff_fluentform_pro_license'] p"))
+        {
+            $this->click('Activate Now', "div[class='error error_notice_ff_fluentform_pro_license'] p");
+            $this->waitForElement("input[name='_ff_fluentform_pro_license_key']",30);
+            $this->fillField("input[name='_ff_fluentform_pro_license_key']",'97019eb28ede80342d3a17da2da5f41d');
+            $this->click("input[value='Activate License']");
+            $this->waitForText('Your license is active! Enjoy Fluent Forms Pro Add On');
+            $this->see('Your license is active! Enjoy Fluent Forms Pro Add On');
+        }
+        $this->amOnPage('/wp-admin/plugins.php');
+        $this->see('Fluent Forms Pro Add On Pack');
+    }
+
+    public function installFluentFormPdfGenerator()
+    {
+        $this->wantTo('Install FluentForm PDF Generator plugin');
+        $this->amOnPage('wp-admin/plugin-install.php?s=Fluent+Forms+PDF+Generator&tab=search&type=term');
+        if($this->tryToSee('Install Now', '.plugin-card.plugin-card-fluentforms-pdf'))
+        {
+            $this->click('Install Now', '.plugin-card.plugin-card-fluentforms-pdf');
+            $this->waitForText('Activate',30,'.plugin-card.plugin-card-fluentforms-pdf');
+            $this->click('Activate', '.plugin-card.plugin-card-fluentforms-pdf');
+        }
+        $this->amOnPage('/wp-admin/plugins.php');
+        $this->see('Fluent Forms PDF Generator');
+    }
+
+    public function uninstallFluentFormPro()
+    {
+        //delete fluentform pro
+        $this->click('Deactivate', "tr[data-slug='fluent-forms-pro-add-on-pack'] td[class='plugin-title column-primary']");
+        $this->waitForText('Plugin deactivated.',30,"div[id='message'] p");
+        $this->see('Plugin deactivated.',"div[id='message'] p");
+        $this->click('Delete', "tr[data-slug='fluent-forms-pro-add-on-pack'] td[class='plugin-title column-primary']");
+        $this->tryToAcceptPopup();
+        $this->waitForText('Fluent Forms Pro Add On Pack was successfully deleted.');
+        $this->see('Fluent Forms Pro Add On Pack was successfully deleted.');
+    }
+
+    public function uninstallFluentFormPdfGenerator()
+    {
+        //delete fluentform pdf generator
+        $this->click('Deactivate', "tr[data-slug='fluentforms-pdf'] td[class='plugin-title column-primary']");
+        $this->waitForText('Plugin deactivated.',30,"div[id='message'] p");
+        $this->see('Plugin deactivated.',"div[id='message'] p");
+        $this->click('Delete', "tr[data-slug='fluentforms-pdf'] td[class='plugin-title column-primary']");
+        $this->tryToAcceptPopup();
+        $this->waitForText('Fluent Forms PDF Generator was successfully deleted.');
+        $this->see('Fluent Forms PDF Generator was successfully deleted.');
+    }
+
+    public function uninstallFluentForm()
+    {
+        //delete fluentform
+        $this->click('Deactivate', "tr[data-slug='fluentform'] td[class='plugin-title column-primary']");
+        $this->waitForText('Plugin deactivated.',30,"div[id='message'] p");
+        $this->see('Plugin deactivated.',"div[id='message'] p");
+        $this->click('Delete', "tr[data-slug='fluentform'] td[class='plugin-title column-primary']");
+        $this->tryToAcceptPopup();
+        $this->waitForText('Fluent Forms was successfully deleted.');
+        $this->see('Fluent Forms was successfully deleted.');
+    }
+
+
 }
