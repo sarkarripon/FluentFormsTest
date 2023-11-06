409ea8ab9b3303a9ae14599e7da246e9e4c9a14b
Sarkar Ripon <sarkarripon@student.sust.edu>
2023-11-06 18:00:11 +0600

    working on dropdown

--- tests/GeneralFields/NumericFieldCest.php
+++ tests/GeneralFields/NumericFieldCest.php
@@ -84,7 +84,47 @@ class NumericFieldCest
         $I->seeElement("//input", ['data-name' => $nameAttribute], $I->cmnt('Check numericField name attribute'));
         $I->seeElement("//div[contains(@class,'$containerClass')]", [], $I->cmnt('Check numericField container class'));
         $I->seeElement("//input[contains(@class,'$elementClass')]", [], $I->cmnt('Check numericField element class'));
+
+
+        $I->seeElement("//input", ['min' => $minValue], $I->cmnt('Check numericField min value'));
+        $I->seeElement("//input", ['max' => $maxValue], $I->cmnt('Check numericField max value'));
+        $I->seeElement("//input", ['step' => 'any'], $I->cmnt('Check numericField step value'));
+
         echo $I->cmnt("All test cases went through. ", 'yellow','',array('blink'));
 
+        // checking error message remaining, because of the bug in the plugin
     }
+    public function test_numeric_field_with_calculation(AcceptanceTester $I)
+    {
+        $pageName = __FUNCTION__ . '_' . rand(1, 100);
+        $faker = \Faker\Factory::create();
+
+        $elementLabel = $faker->words(2, true);
+        $adminFieldLabel = $faker->words(2, true);
+        $placeholder = $faker->words(3, true);
+        $requiredMessage = $faker->words(2, true);
+
+        $defaultValue = $faker->words(2, true);
+        $containerClass = $faker->firstName();
+        $elementClass = $faker->userName();
+        $helpMessage = $faker->words(4, true);
+        $prefixLabel = $faker->words(2, true);
+        $suffixLabel = $faker->words(3, true);
+        $nameAttribute = $faker->firstName();
+        $minValue = $faker->numberBetween(10, 50);
+        $maxValue = $faker->numberBetween(60, 99);
+        $digits = $faker->numberBetween(2, 2);
+
+        $customName = [
+            'numericField' => $elementLabel,
+        ];
+
+        $this->prepareForm($I, $pageName, [
+            'generalFields' => ['numericField'],
+        ], true, $customName);
+
+
+    }
+
+
 }
